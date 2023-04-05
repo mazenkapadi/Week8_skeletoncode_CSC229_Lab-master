c2499d9e98e859c29f5ce05454dbc182fb67c998
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-04-05 11:15:48 -0400

    Completed Problem01

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.week8_skeletoncode_lab;
+import java.util.*;
 
 /**
  *
@@ -11,10 +12,26 @@ package com.mycompany.week8_skeletoncode_lab;
 public class Problem01 {
     
     public static long getSumOfPrimes(int n){
-    // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
-    //          Test your solution
-    //          Analyze its space and time  
-        return 0;
+        int sum = 0;
+        for(int i = 2; i <= n; i++){
+            boolean isPrime = true;
+            for(int j = 2; j < i; j++){
+                if(i%j == 0 ) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                sum += i;
+            }
+        }
+        return sum;
     }
-    
-}
+
+    public static void main(String[] args) {
+        System.out.println(getSumOfPrimes(10)); //Output should be 17
+        System.out.println(getSumOfPrimes(20)); //Output should be 77
+    }
+
+    //the space complexity of this method is O(n/log(n)).
+}   //the time complexity of this method is O(n*log(n))
